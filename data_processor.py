5bd4e6580dbcdc1103ca78c08c6683aa327eb8bd
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2025-10-03 15:20:52 +0200

    data_df

--- data_processor.py
+++ data_processor.py
@@ -6,20 +6,17 @@ import matplotlib.pyplot as plt
 # Load .mat file
 mat_file = scipy.io.loadmat("Flyover_No_5.mat")
 
-
 variables = mat_file.keys()
 print(variables)
 
 # Extract the variable you care about
 data = mat_file["sound_pressure"]
 
-# Convert to DataFrame
 # Convert to DataFrame (flatten to 1D)
 df = pd.DataFrame(data.flatten(), columns=["amplitude"])
 
 # Make an index column representing frequency bins
-df["frequency_bin"] = df.index                          # x = df["frequency_bin"], y = df["amplitude"]
-
+df["samples"] = df.index                          # x = df["samples"], y = df["amplitude"]
 
 
 if '__main__':
@@ -29,10 +26,10 @@ if '__main__':
 
     # Plot spectrum
     plt.figure(figsize=(12,6))
-    plt.plot(df["frequency_bin"], df["amplitude"], linewidth=0.7)
+    plt.plot(df["samples"], df["amplitude"], linewidth=0.7)
 
     plt.title("Spectrum of Sound Pressure")
-    plt.xlabel("Frequency Bin")
+    plt.xlabel("Samples")
     plt.ylabel("Amplitude")
     plt.grid(True)
     plt.tight_layout()
