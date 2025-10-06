c398d9a6853c25357ab49c0ac346191b93b83db7
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2025-10-06 13:54:19 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from data_processor import df
 import numpy as np
 
-
 sampling_frequency = 40000
 N_samples = len(df.index)
-chunk_length = 0.1
+chunk_duration = 0.1
 
 # 1. Load aircraft noise data file 
 
@@ -15,26 +14,27 @@ print(f"duration: {duration}")
 
 
 # 3. How many samples correspond to a data lengt of T 0.1 seconds
-samples_per_chunck = sampling_frequency * chunk_length
+samples_per_chunck = sampling_frequency * chunk_duration
 print(f"samples_per_chunck: {samples_per_chunck}")
 
 
 # 4. Calculate the so-called  ́effective sound pressure ́ as a function of time using equation 1.4 from the lecture notes
-# basis staat maar is nog niet helemaal af
+# x_2_data is a list containing the squared average pressure for each chunk
 
-x_2_data = []
+effective_pressures = []
 
-iterations = int(np.ceil((N_samples/samples_per_chunck)))
-print(iterations)
+sample_iterations = int(np.ceil((N_samples/samples_per_chunck)))
 
-for i in range(iterations):
+for i in range(sample_iterations):
     start_idx = int(i * samples_per_chunck)
     end_idx = int(min(start_idx + samples_per_chunck, N_samples))
-    chunk = df[start_idx:end_idx]
-    
-    # Calculating effective pressure
+    #print(f"start_idx: {start_idx}, end_idx: {end_idx}")
 
+    chunk = df[start_idx:end_idx]           # x(t) waardes van de chunk
+
+    # appending chunk to x_2_data set
     avg_x = np.average(chunk)
-    x_2_data.append(avg_x**2)
+    effective_pres_dt = 1/chunk_duration*np.sum(chunk['amplitude']**2)
 
-effective_pres = 1/duration*sum(x_2_data)
+    effective_pressures.append(effective_pres_dt)
+
